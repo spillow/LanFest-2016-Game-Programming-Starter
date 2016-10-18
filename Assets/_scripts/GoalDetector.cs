9d087ff19c1eadc7c99bfcb31338550eaded80b5
spillow <scott.pillow@gmail.com>
2016-10-18 00:22:04 -0700

    Added goal check.

---
+++ Assets/_scripts/GoalDetector.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public enum GoalColor
+{
+    Blue,
+    Orange
+}
+
+public class GoalDetector : MonoBehaviour
+{
+    public GameObject m_Ball;
+    public GoalColor m_GoalColor;
+
+    public Text GoalText;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == m_Ball.tag)
+        {
+            GoalText.text = "Goal Scored!";
+        }
+    }
+}
